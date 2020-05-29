711eb5f90900c30ebbe90442ebd674a95fb6b115
Nikolay Radonich <nikolayradonich@gmail.com>
2020-05-30 00:17:48 +0300

    added form validation; added request sending; added request result notification; added form loader;

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -1,21 +1,66 @@
 <template>
-  <div class="contact-us">
+  <section class="contact-us">
     <h2>Contact us</h2>
+
+    <div
+      :class="[
+        'result-message',
+        `__${resultMessage.type}`,
+        { 'result-message__active': resultMessage.show },
+      ]"
+    >
+      <p class="result-title">
+        {{ resultMessage.title }}
+      </p>
+      <p class="result-description">
+        {{ resultMessage.description }}
+      </p>
+    </div>
 
     <div class="container">
       <div class="contact-us-form">
         <form method="POST" @submit.prevent="handleSubmit">
-          <form-group label="Name" type="text" />
-          <form-group label="Phone" type="text" />
-          <form-group label="Email" type="email" />
-          <form-group
-            label="I agree the processing of personal data"
-            type="checkbox"
-          />
+          <loader :loading="sendingData">
+            <form-group
+              label="Name"
+              type="text"
+              validation-type="name"
+              :error="error.name"
+              @input="inputFields"
+            />
+            <form-group
+              label="Phone"
+              type="text"
+              validation-type="phone"
+              :error="error.phone"
+              @input="inputFields"
+            />
+            <form-group
+              label="Email"
+              type="email"
+              validation-type="email"
+              :error="error.email"
+              @input="inputFields"
+            />
+            <form-group
+              label="I agree the processing of personal data"
+              type="checkbox"
+              validation-type="terms"
+              :error="error.terms"
+              @input="inputFields"
+            />
 
-          <button type="submit" class="contact-us-submit">
-            get in touch
-          </button>
+            <button
+              type="submit"
+              :class="[
+                'contact-us-submit',
+                { 'contact-us-submit__disabled': sendingData },
+              ]"
+              :disabled="sendingData"
+            >
+              get in touch
+            </button>
+          </loader>
         </form>
       </div>
       <div class="contact-us-text">
@@ -25,19 +70,125 @@
         </p>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 import FormGroup from '@/components/FormGroup';
+import Loader from '@/components/Loader';
 
 export default {
   components: {
     FormGroup,
+    Loader,
+  },
+  data() {
+    return {
+      resultMessage: {
+        type: '',
+        title: '',
+        description: '',
+      },
+      sendingData: false,
+      fieldData: {
+        name: '',
+        phone: '',
+        email: '',
+        terms: false,
+      },
+      error: {
+        name: '',
+        phone: '',
+        email: '',
+        terms: '',
+      },
+    };
   },
   methods: {
-    handleSubmit() {
-      // console.log('success!');
+    handleSubmit(e) {
+      const data = { ...this.fieldData };
+
+      Object.entries(data).forEach(([key, value]) => {
+        this.error[key] = this.validateFields(value, key);
+      });
+
+      if (Object.values(this.error).every(v => v === '')) {
+        this.sendingData = true;
+
+        this.$axios
+          .post('http://httpbin.org/post', data)
+          .then(res => {
+            console.log(
+              'Your result is here',
+              res && res.data && res.data.json
+            );
+
+            this.resultMessage = {
+              show: true,
+              type: 'success',
+              title: 'Success!',
+              description: 'Message has been set successfully!',
+            };
+          })
+          .catch(() => {
+            this.resultMessage = {
+              show: true,
+              type: 'error',
+              title: 'Fail!',
+              description: 'An error occured. Please, try later',
+            };
+          })
+          .finally(() => {
+            setTimeout(() => {
+              this.sendingData = false;
+            }, 300);
+
+            setTimeout(() => {
+              this.resultMessage = {
+                show: false,
+              };
+            }, 1500);
+          });
+      }
+    },
+
+    inputFields(val, validationType) {
+      this.fieldData[validationType] = val;
+    },
+
+    validateFields(val, validationType) {
+      let regex = '';
+      let message = '';
+
+      switch (validationType) {
+        case 'name':
+          regex = /^[a-zA-Z ]+$/gm;
+          message = 'Please, input valid name';
+
+          break;
+
+        case 'phone':
+          regex = /^((\+380)+([0-9]){9})$/gm;
+          message = 'Please, input valid phone number';
+
+          break;
+
+        case 'email':
+          regex = /\S+@\S+\.\S+/gm;
+          message = 'Please, input valid email';
+
+          break;
+      }
+
+      let fieldError;
+
+      if (validationType !== 'terms') {
+        fieldError = regex.test(val) ? '' : message;
+      } else {
+        fieldError = val ? '' : 'Please, accept the processing data terms';
+      }
+
+      return fieldError;
     },
   },
 };
@@ -65,6 +216,12 @@ export default {
     width: 50%;
   }
 
+  &-form {
+    form {
+      max-width: $form-width;
+    }
+  }
+
   &-text {
     font-weight: 600;
   }
@@ -74,7 +231,7 @@ export default {
     background-color: $grey;
     border: 0;
     outline: none;
-    transition: background-color 0.3s ease;
+    transition: all 0.3s ease;
     padding: 20px 40px;
     text-transform: uppercase;
     font-family: 'Suisse SemiBold';
@@ -86,6 +243,53 @@ export default {
     &:hover {
       background-color: $dark-grey;
       color: darken($white, 5);
+    }
+
+    &__disabled {
+      background-color: #6d6d6d;
+      color: #bfbfbf;
+
+      &:hover {
+        background-color: #6d6d6d;
+        color: #bfbfbf;
+      }
+    }
+  }
+
+  .result {
+    &-message {
+      display: block;
+      position: fixed;
+      top: 20px;
+      right: -100vw;
+      padding: $spacer / 2;
+      z-index: 99999;
+      transition: right 0.5s ease;
+      background-color: $white;
+      box-shadow: 1px 1px 6px -3px $grey;
+      border-radius: 10px;
+
+      &.__success {
+        color: $green;
+      }
+
+      &.__error {
+        color: $red;
+      }
+
+      &__active {
+        right: 10px;
+      }
+    }
+
+    &-title {
+      margin-bottom: 15px;
+      font-weight: 600;
+      font-size: 1em;
+    }
+
+    &-description {
+      font-size: 0.8em;
     }
   }
 }
--- components/FormGroup.vue
+++ components/FormGroup.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="form-group">
-    <input
-      :id="label"
-      :type="type"
-      :class="[
-        { 'form-input': type !== 'checkbox' },
-        { 'form-checkbox': type === 'checkbox' },
-      ]"
-      required
-    />
-    <label :for="label" :class="{ 'input-label': type !== 'checkbox' }">{{
-      label
-    }}</label>
-    <div v-if="type !== 'checkbox'" class="form-line"></div>
+  <div>
+    <div class="form-group">
+      <input
+        :id="label"
+        :type="type"
+        :class="[
+          { 'form-input': type !== 'checkbox' },
+          { 'form-checkbox': type === 'checkbox' },
+          { 'form-input__error': !!error },
+        ]"
+        required
+        @change="onChange"
+      />
+      <label :for="label" :class="{ 'input-label': type !== 'checkbox' }">{{
+        label
+      }}</label>
+      <div v-if="type !== 'checkbox'" class="form-line"></div>
+    </div>
+
+    <p v-show="!!error" class="form-error">{{ error }}</p>
   </div>
 </template>
 
@@ -21,6 +27,17 @@ export default {
   props: {
     label: { required: true, type: String },
     type: { required: true, type: String },
+    validationType: { required: true, type: String },
+    error: { required: true, type: String },
+  },
+  methods: {
+    onChange(e) {
+      this.$emit(
+        'input',
+        this.type === 'checkbox' ? e.target.checked : e.target.value,
+        this.validationType
+      );
+    },
   },
 };
 </script>
@@ -31,19 +48,8 @@ export default {
 .form {
   &-group {
     position: relative;
-    max-width: 320px;
+    max-width: $form-width;
     margin: $spacer / 3 0;
-
-    &:hover {
-      .input-label {
-        color: $white;
-      }
-
-      .form-line {
-        background-color: $white;
-        width: 100%;
-      }
-    }
 
     label {
       font-size: 0.8em;
@@ -62,6 +68,17 @@ export default {
         font-size: 1em;
       }
     }
+
+    &:hover {
+      .input-label {
+        color: $white;
+      }
+
+      .form-line {
+        background-color: $white;
+        width: 100%;
+      }
+    }
   }
 
   &-input {
@@ -78,6 +95,31 @@ export default {
         color: $white;
 
         + .form-line {
+          width: 100%;
+        }
+      }
+    }
+
+    &__error {
+      + label {
+        &.input-label {
+          color: $red;
+
+          + .form-line {
+            background-color: $red;
+            width: 100%;
+          }
+        }
+      }
+
+      &:hover,
+      &:focus {
+        + .input-label {
+          color: darken($red, 20);
+        }
+
+        + .form-line {
+          background-color: darken($red, 20);
           width: 100%;
         }
       }
@@ -101,6 +143,13 @@ export default {
     transition: 0.3s width ease-in-out;
     bottom: 0;
     left: 0;
+  }
+
+  &-error {
+    color: $red;
+    font-family: 'Suisse SemiBold';
+    font-size: 11px;
+    margin-top: 5px;
   }
 }
 </style>
